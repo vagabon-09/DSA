e13c12c3824572acbd8a84761d07956abe58b357
Sand Man <rajeshbhadra62@gmail.com>
2022-10-30 23:06:17 +0530

    Add beginning operation is adding

--- Singly linked list/addbeginning.c
+++ Singly linked list/addbeginning.c
@@ -7,6 +7,49 @@ typedef struct singly
 
 Node *Head = NULL;
 void display();
+void  add_beginning(int value);
 int main(){
+    while (1)
+    {
+        int value;
+        char check;
+        printf("Do you want to create a list(Y/N)?");
+        scanf("%c",&check);
+        fflush(stdin);
+        if (check == 'Y' || check == 'y')
+        {
+            printf("Enter value: ");
+            scanf("%d",&value);
+            fflush(stdin);
+            add_beginning(value);
+        }else
+        {
+            break;
+        }
+        
+        
+    }
+    display();
+    
+}
+
+void add_beginning(int value){
+    Node *newptr, *ptr;
+    newptr = (Node*)malloc(sizeof(Node));
+    newptr->data = value;
+    newptr->link = NULL;
+    if (Head == NULL)
+    {
+       Head = newptr;
+    }else
+    {
+        ptr = Head;
+        while (ptr->link!=NULL)
+        {
+            
+        }
+        
+    }
+    
     
 }
