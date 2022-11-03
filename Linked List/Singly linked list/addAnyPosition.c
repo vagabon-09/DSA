055a228e5feff64cb3f63a09800cc2ffe9c6029a
Sand Man <rajeshbhadra62@gmail.com>
2022-11-03 21:14:09 +0530

    linked list node numbers are counting added

--- Linked List/Singly linked list/addAnyPosition.c
+++ Linked List/Singly linked list/addAnyPosition.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int position = 0;
 typedef struct singly
 {
     int data;
@@ -8,11 +9,11 @@ typedef struct singly
 Node *Head = NULL;
 void display();
 void create_node(int value);
-void add_anyPosition(int item,int n);
+void add_anyPosition(int n);
 
 int main()
 {
-    int value;
+    int value , item , n;
     char choice;
     while (1)
     {
@@ -26,45 +27,72 @@ int main()
             scanf("%d", &value);
             fflush(stdin);
             create_node(value);
-
-        }else
+        }
+        else
         {
+            while (1)
+            {
+                printf("Do you want to insert value(Y/N)?");
+                scanf("%c",&choice);
+                if(choice == 'y' || choice == 'Y'){
+                    printf("Enter value: ");
+                    scanf("%d",&item);
+                    printf("Enter position: ");
+                    scanf("%d",&n);
+                    add_anyPosition(n);
+                    fflush(stdin);
+                }else
+                {
+                    break;
+                }
+                
+            }
+            
             break;
         }
-        
     }
     display();
 }
 
-void create_node(int value){
-    Node *newptr,*ptr;
-    newptr =(Node*)malloc(sizeof(Node));
+void create_node(int value)
+{
+    position++;
+    Node *newptr, *ptr;
+    newptr = (Node *)malloc(sizeof(Node));
     newptr->data = value;
     newptr->link = NULL;
     if (Head == NULL)
     {
         Head = newptr;
-    }else
+    }
+    else
     {
         ptr = Head;
-        while (ptr->link!=NULL)
+        while (ptr->link != NULL)
         {
             ptr = ptr->link;
         }
         ptr->link = newptr;
-        
     }
-    
 }
 
-void display(){
+void display()
+{
     Node *view;
     view = Head;
-    while (view!=NULL)
+    while (view != NULL)
     {
-        printf("%d -> ",view->data);
-        view = view ->link;
+        printf("%d -> ", view->data);
+        view = view->link;
     }
     printf("NULL");
-    
+}
+
+void add_anyPosition(int n)
+{
+    if (position < n)
+    {
+        printf("Invalid position..\n");
+        return;
+    }
 }
