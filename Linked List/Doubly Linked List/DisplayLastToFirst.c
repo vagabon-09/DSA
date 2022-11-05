c9cc9ffa3d128d117e4d81b6410aa9a769ed3858
Sand Man <rajeshbhadra62@gmail.com>
2022-11-06 03:01:53 +0530

    travers from last to first in doubely linked list

---
+++ Linked List/Doubly Linked List/DisplayLastToFirst.c
@@ -0,0 +1,22 @@
+// In this code we will learn how to display a doubely linked list from last to first
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct singly
+{
+    int data;
+    struct singly *next;
+    struct sigly *prev;
+} Node;
+
+Node *Head = NULL;
+Node *Last = NULL;
+void create_list(int data);
+void display();
+
+int main(){
+    while (1)
+    {
+        /* code */
+    }
+    
+}
