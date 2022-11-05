801fe62a8f529efa1cbb871b8949631abbd4edf3
Sand Man <rajeshbhadra62@gmail.com>
2022-11-06 03:10:30 +0530

    travers node last to first

--- Linked List/Doubly Linked List/DisplayLastToFirst.c
+++ Linked List/Doubly Linked List/DisplayLastToFirst.c
@@ -13,10 +13,62 @@ Node *Last = NULL;
 void create_list(int data);
 void display();
 
-int main(){
+int main()
+{
+    int data;
+    char choice;
     while (1)
     {
-        /* code */
+        printf("Do you want to create list(Y/N)?");
+        scanf("%c", &choice);
+        fflush(stdin);
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("Enter data: ");
+            scanf("%d", &data);
+            fflush(stdin);
+            create_list(data);
+        }
+        else
+        {
+            break;
+        }
     }
+    display();
+}
+
+void create_list(int data)
+{
+    Node *newPtr, *ptr;
+    newPtr = (Node *)malloc(sizeof(Node));
+    newPtr->data = data;
+    newPtr->next = NULL;
+    newPtr->prev = NULL;
+    if (Head == NULL)
+    {
+        Head = newPtr;
+    }
+    else
+    {
+        ptr = Head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = newPtr;
+        newPtr->prev = ptr;
+        Last = newPtr;
+    }
+}
+
+void display(){
+    Node *ptr;
+    ptr = Last;
+    while (ptr!=NULL)
+    {
+        printf("%d -> ",ptr->data);
+        ptr = ptr->prev;
+    }
+    printf("NULL");
     
 }
