a82d6464aac78790b5fde6442a7943c3c282d478
Sand Man <rajeshbhadra62@gmail.com>
2022-11-07 21:18:21 +0530

    first commit

---
+++ Linked List/Doubly Linked List/addAnyPosition.c
@@ -0,0 +1,37 @@
+// In this program we will lear how to add Node at any position
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct doubely
+{
+    int data;
+    struct doubely *next;
+    struct doubley *prev;
+} Node;
+
+Node *head = NULL;
+Node *last = NULL;
+void display();
+void createList(int value);
+void insertAt(int item, int value);
+int main()
+{
+    int data;
+    char choice;
+    while (1)
+    {
+        printf("Do you want to create list(Y/N)?");
+        scanf("%c", &choice);
+        fflush(stdin);
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("Enter value: ");
+            scanf("%d", &data);
+            fflush(stdin);
+        }
+        else
+        {
+            break;
+        }
+    }
+    display();
+}
