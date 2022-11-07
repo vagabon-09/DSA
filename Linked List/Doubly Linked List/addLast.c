44f21ac70e93709ff037a10b2c56be6da81d14e6
Sand Man <rajeshbhadra62@gmail.com>
2022-11-07 20:25:50 +0530

    not done yet

--- Linked List/Doubly Linked List/addLast.c
+++ Linked List/Doubly Linked List/addLast.c
@@ -4,8 +4,8 @@
 typedef struct doubely
 {
     int data;
-    struct doubley *next;
-    struct doubley *prev;
+    struct doubely *next;
+    struct doubely *prev;
 } Node;
 
 Node *Head = NULL;
@@ -20,38 +20,84 @@ int main()
     while (1)
     {
         printf("Do you want to create list(Y/N)?");
-        scanf("%c",&choice);
+        scanf("%c", &choice);
         fflush(stdin);
-        if (choice == 'y' || choice =='Y')
+        if (choice == 'y' || choice == 'Y')
         {
             printf("Enter value: ");
-            scanf("%d",&data);
+            scanf("%d", &data);
             fflush(stdin);
             createList(data);
-        }else
+        }
+        else
         {
             while (1)
             {
                 printf("Do you want to add last(Y/N)?");
-                scanf("%c",&choice);
+                scanf("%c", &choice);
                 fflush(stdin);
-                if(choice == 'y' || choice == 'Y'){
+                if (choice == 'y' || choice == 'Y')
+                {
                     printf("Enter value: ");
-                    scanf("%d",&data);
+                    scanf("%d", &data);
                     fflush(stdin);
                     addLast(data);
-                }else
+                }
+                else
                 {
                     break;
                 }
-                
             }
-            
+
             break;
         }
-        
-        
     }
     display();
-    
+}
+
+void createList(int value)
+{
+    Node *newPtr, *ptr;
+    newPtr = (Node *)malloc(sizeof(Node));
+    newPtr->data = value;
+    newPtr->next = NULL;
+    newPtr->prev = NULL;
+    if (Head == NULL)
+    {
+        Head = newPtr;
+    }
+    else
+    {
+        ptr = Head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = newPtr;
+        newPtr->next = ptr;
+        Last = newPtr;
+    }
+}
+
+void addLast(int value2)
+{
+    Node *add, *newPtr;
+    add = Last;
+    newPtr = (Node *)malloc(sizeof(Node));
+    newPtr->data = value2;
+    newPtr->next = NULL;
+    newPtr->prev = add;
+    Last = newPtr;
+}
+
+void display()
+{
+    Node *start;
+    start = Head;
+    while (start != NULL)
+    {
+        printf("%d -> ", start->data);
+        start = start->next;
+    }
+    printf("NULL");
 }
