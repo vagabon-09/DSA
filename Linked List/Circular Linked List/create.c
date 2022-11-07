2b5ad0783c5013c81535ba219a5ab40effb737bc
Sand Man <rajeshbhadra62@gmail.com>
2022-11-07 23:48:47 +0530

    working ..(Not done yet)

---
+++ Linked List/Circular Linked List/create.c
@@ -0,0 +1,69 @@
+// In this program we will learn how to create a circular linked list using singly linked list
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct circular
+{
+    int data;
+    struct circular *link;
+} Node;
+Node *head = NULL;
+void display();
+void createList(int);
+int main()
+{
+    int data;
+    char choice;
+    while (1)
+    {
+        printf("Do you want to create a list(Y/N)");
+        scanf("%c", &choice);
+        fflush(stdin);
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("Enter data: ");
+            scanf("%d", &data);
+            fflush(stdin);
+            createList(data);
+        }
+        else
+        {
+            break;
+        }
+    }
+    display();
+}
+
+void createList(int value){
+    Node *newPtr,*ptr;
+    newPtr = (Node*)malloc(sizeof(Node));
+    newPtr->data = value;
+    newPtr->link = NULL;
+   if (head == NULL)
+   {
+    head = newPtr;
+   }else
+   {
+    ptr = head;
+    while (ptr->link!=NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = newPtr;
+    
+   }
+   
+   
+    
+}
+
+void display()
+{
+    Node *start;
+    start = head;
+    while (start != NULL)
+    {
+        printf("%d -> ", start->data);
+        start = start->link;
+    }
+    printf("NULL");
+}
