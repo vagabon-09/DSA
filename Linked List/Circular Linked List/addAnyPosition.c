6adc69e626f0300dbcaab7ff55bd89b86379f612
Sand Man <rajeshbhadra62@gmail.com>
2022-11-08 19:45:24 +0530

    program is working but update need

--- Linked List/Circular Linked List/addAnyPosition.c
+++ Linked List/Circular Linked List/addAnyPosition.c
@@ -12,7 +12,7 @@ int index = 0;
 void display();
 void createList(int);
 void circular();
-void addAtAnyPosition(int);
+void addAtAnyPosition(int, int);
 
 int main()
 {
@@ -32,11 +32,56 @@ int main()
         }
         else
         {
+            int position;
+            while (1)
+            {
+                printf("Do you want ot add at anypositoin(Y/N) ?");
+                scanf("%c", &choice);
+                fflush(stdin);
+                if (choice == 'y' || choice == 'Y')
+                {
+                    printf("Position: ");
+                    scanf("%d", &position);
+                    if (position > index || position < 0)
+                    {
+                        printf("Invalid positon \n");
+                    }
+                    else
+                    {
+                        printf("Enter item :");
+                        scanf("%d", &data);
+                        fflush(stdin);
+                        addAtAnyPosition(data, position);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
             break;
         }
     }
     circular();
     display();
+}
+
+void addAtAnyPosition(int item, int position)
+{
+    int i = 1;
+    Node *ptr, *nxtPtr, *newPtr;
+    newPtr = (Node *)malloc(sizeof(Node));
+    newPtr->data = item;
+    ptr = head;
+    while (ptr->link != NULL && i < position-1)
+    {
+        ptr = ptr->link;
+        i++;
+    }
+    nxtPtr = ptr->link;
+    ptr->link = newPtr;
+    newPtr->link = nxtPtr;
 }
 
 void circular()
