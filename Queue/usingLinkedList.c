f07db6e594ee3f4a39598df85fb351e1c12c7618
Sand Man <rajeshbhadra62@gmail.com>
2022-11-09 23:04:17 +0530

    Linked list is done

--- Queue/usingLinkedList.c
+++ Queue/usingLinkedList.c
@@ -11,5 +11,62 @@ Node *front = NULL;
 Node *read = NULL;
 
 void display();
-void createList(int);
+void createQueue(int);
 void enqueue(int);
+
+int main()
+{
+    int data;
+    char choice;
+    while (1)
+    {
+        printf("Do you want to create a Queue(y/n)?");
+        scanf("%c", &choice);
+        fflush(stdin);
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("Enter data:");
+            scanf("%d", &data);
+            fflush(stdin);
+            createQueue(data);
+        }
+        else
+        {
+            break;
+        }
+    }
+    display();
+}
+
+void createQueue(int data)
+{
+    Node *newptr, *ptr;
+    newptr = (Node *)malloc(sizeof(Node));
+    newptr->data = data;
+    newptr->link = NULL;
+    if (front == NULL)
+    {
+        front = newptr;
+    }
+    else
+    {
+        ptr = front;
+        while (ptr->link != NULL)
+        {
+            ptr = ptr->link;
+        }
+        ptr->link = newptr;
+    }
+}
+
+void display()
+{
+    Node *ptr;
+    ptr = front;
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("NULL");
+}
