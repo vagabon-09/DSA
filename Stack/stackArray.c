983243516241ab1fffd6f1cd6b8c4ec2864454df
Sand Man <rajeshbhadra62@gmail.com>
2022-11-11 14:14:39 +0530

    working...

---
+++ Stack/stackArray.c
@@ -0,0 +1,9 @@
+//In this program we will see how to create a stack using array
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter size of the Stack");
+    scanf("%d",&n);
+    int stack[n];
+    
+}
