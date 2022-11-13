ac223f3299a2934d0516ed630be0fb839f532fb5
Sand Man <rajeshbhadra62@gmail.com>
2022-11-13 22:47:21 +0530

    creating stack using array

--- Stack/stackArray.c
+++ Stack/stackArray.c
@@ -2,7 +2,53 @@
 #include <stdio.h>
 int data = 0;
 int arr[5];
+int top = -1;
+void createStack(int);
+void display();
 
 int main()
 {
+    int data;
+    char choice;
+    while (1)
+    {
+        printf("Do you want to create stack(Y/N)?");
+        scanf("%c", &choice);
+        fflush(stdin);
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("Enter data: ");
+            scanf("%d", &data);
+            createStack(data);
+            fflush(stdin);
+        }
+        else
+        {
+            break;
+        }
+    }
+    display();
+}
+
+void createStack(int data)
+{
+    top++;
+    if (top > 5)
+    {
+        printf("Error: Stack overfollow...");
+    }
+    else
+    {
+
+        arr[top] = data;
+    }
+}
+
+void display(){
+    printf("Available data in stack is: ");
+    for (int i = 0; i <= top; i++)
+    {
+        printf("%d",arr[i]);
+    }
+    
 }
