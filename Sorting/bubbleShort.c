41259629c2963d33ee4c828f07f6c49448763841
Sand Man <rajeshbhadra62@gmail.com>
2022-11-16 15:02:04 +0530

    bubble short is working now...

--- Sorting/bubbleShort.c
+++ Sorting/bubbleShort.c
@@ -3,22 +3,22 @@
 int arr[]={15,16,6,8,5};
 void shoring();
 int main(){
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5-1; i++)
     {
-        for (int j = 0; j < 5-i; j++)
+        for (int j = 0; j < 5-i-1; j++)
         {
-            if (arr[i] > arr[i+1])
+            if (arr[j] > arr[j+1])
             {
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
             
         }
         
     }
 
-    for (int i = 1; i <= 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         printf("%d ",arr[i]);
     }
