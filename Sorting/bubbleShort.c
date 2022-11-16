56ed137728d0b166c1ca36f5cd2dc58a043292e5
Sand Man <rajeshbhadra62@gmail.com>
2022-11-16 15:26:40 +0530

    program is working....

--- Sorting/bubbleShort.c
+++ Sorting/bubbleShort.c
@@ -1,28 +1,46 @@
 // In this vedio we will see how to program a bubble short in c
-#include<stdio.h>
-int arr[]={15,16,6,8,5};
-void shoring();
-int main(){
-    for (int i = 0; i < 5-1; i++)
+#include <stdio.h>
+
+void shorting(arr, n);
+void display();
+int main()
+{
+    int n;
+    printf("Enter the size of array : ");
+    scanf("%d", &n);
+    int arr[n];
+    shorting(arr, n);
+    display(arr, n);
+}
+
+void shorting(int arr[], int n)
+{
+    // Scanning data form users
+    printf("Enter values in array: ");
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 5-i-1; j++)
+        scanf("%d", &arr[i]);
+    }
+    // Shorting values from array
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] > arr[j+1])
+            if (arr[j] > arr[j + 1])
             {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-            
         }
-        
     }
+}
 
-    for (int i = 0; i < 5; i++)
+void display(int arr[], int n)
+{
+    printf("Shorted value is : \n");
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d\n", arr[i]);
     }
-    
-    
-    
 }
