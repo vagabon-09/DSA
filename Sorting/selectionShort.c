803f4b33eb90a8b1d41881aab46e0da53347b337
Sand Man <rajeshbhadra62@gmail.com>
2022-11-26 11:20:27 +0530

    final commite done

--- Sorting/selectionShort.c
+++ Sorting/selectionShort.c
@@ -7,30 +7,26 @@ int main()
 {
     int arr[] = {20, 35, -15, 7, 55, 1, -22};
     int n = 7;
-    int min = 0,temp;
-    for (int i = 0; i < n-1; i++)
+    int min = 0, temp;
+    for (int i = 0; i < n - 1; i++)
     {
         min = i;
-        for (int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-          if (arr[min] > arr[j])
-          {
-            min = j;
-          }
-          
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
         }
         temp = arr[i];
         arr[i] = arr[min];
         arr[min] = temp;
-
+        // if you want to show the pass then un comment the below two line
+        //  printf("this is %d -> pass\n", i);
+        //  display(arr);
     }
     display(arr);
-    
-    
-    
 }
-
-
 
 void display(int arr[])
 {
@@ -38,4 +34,5 @@ void display(int arr[])
     {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 }
