4c90cb7f0323c29522db6a0909e09f1326b3d086
Sand Man <rajeshbhadra62@gmail.com>
2022-11-26 11:11:08 +0530

    selection sort is working now ....

--- Sorting/selectionShort.c
+++ Sorting/selectionShort.c
@@ -1,1 +1,41 @@
-//In this program we Z
+// In this program we will apply insertion sort
+#include <stdio.h>
+void display(int arr[]);
+void swap(int arr[], int largest, int lastIndex);
+void selectionSort(int arr[], int data);
+int main()
+{
+    int arr[] = {20, 35, -15, 7, 55, 1, -22};
+    int n = 7;
+    int min = 0,temp;
+    for (int i = 0; i < n-1; i++)
+    {
+        min = i;
+        for (int j = i+1; j < n; j++)
+        {
+          if (arr[min] > arr[j])
+          {
+            min = j;
+          }
+          
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+
+    }
+    display(arr);
+    
+    
+    
+}
+
+
+
+void display(int arr[])
+{
+    for (int i = 0; i <= 6; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
