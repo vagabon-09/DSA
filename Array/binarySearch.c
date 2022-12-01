ba4912cecc6923e17d52c24d72da842519a03664
Sand Man <rajeshbhadra62@gmail.com>
2022-12-02 02:36:59 +0530

    binary search updated..

--- Array/binarySearch.c
+++ Array/binarySearch.c
@@ -1,48 +1,42 @@
+// In this we will see how to apply binary search
 #include <stdio.h>
-int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-int binary_search(int data, int first, int last);
-
-int main()
+int bSearch(int arr[], int low, int high, int key)
 {
-    int data, a;
-    char choice;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Do you want to search (Y/N)?");
-    scanf("%c", &choice);
-    if (choice == 'y' || choice == 'Y')
+    while (low <= high)
     {
-        printf("Enter element : ");
-        scanf("%d", &data);
-        a = binary_search(data, 0, n);
-    }
-    if (a == -1)
-    {
-        printf("\nSorry! Element not found.");
-    }
-    else
-    {
-        printf("\nGreat, Element found at %d position: ", a);
-    }
-}
-
-int binary_search(int data, int first, int last)
-{
-    while (first < last)
-    {
-        int mid = (last + first) / 2;
-        if (arr[mid] == data)
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
         {
             return mid;
         }
-        else if (arr[mid] < data)
-        {
-            first = mid + 1;
-        }
         else
         {
-            last = mid - 1;
+            if (arr[mid] < key)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
         }
     }
     return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 9, 10, 15, 17, 19};
+    int n = sizeof(arr) / sizeof(int);
+    int check, key;
+    printf("Search element: ");
+    scanf("%d", &key);
+    check = bSearch(arr, 0, n, key);
+    if (check == -1)
+    {
+        printf("Data is not found in the present array ");
+    }
+    else
+    {
+        printf("Data is founded at position %d ", check + 1);
+    }
 }
