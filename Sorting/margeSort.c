9d64b7cbcd4afc199b103fe772a857ea0b52570c
Sand Man <rajeshbhadra62@gmail.com>
2022-12-01 22:31:08 +0530

    applying marge short

---
+++ Sorting/margeSort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+int main()
+{
+    int arr[] = {5, 9, 14, 12, 8, 7, 6};
+
+
+
+
+
+
+    
+    display(arr,7);
+}
