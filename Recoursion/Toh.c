491bcf9bb3834607afbf9f0af345f116012fad31
Sand Man <rajeshbhadra62@gmail.com>
2022-12-02 01:13:39 +0530

    tower of honoi is done

---
+++ Recoursion/Toh.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void toh(int n, char s, char t, char d)
+{
+    if (n>0)
+    {
+        toh(n-1,s,d,t);
+        printf("\n Move disc from %c -> %c",s,d);
+        toh(n-1,t,s,d);
+    }
+}
+int main()
+{
+    int disck = 3;
+    toh(disck,'a','b','c');
+}
